d5e9cb1e59b418efe6af5e1d89987ecade8462f5
samsks <samuel.meireles.15s@outlook.com>
2023-10-27 10:50:37 -0300

    ci: testing error case in push action.

--- math_test.go
+++ math_test.go
@@ -5,7 +5,7 @@ import "testing"
 
 
 func TestSum(t *testing.T){
-	total := sumInt(12, 10)
+	total := SumInt(12, 10)
 
 	if total != 22 {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 22)
