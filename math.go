e527ddb254d0fc708de69a278157fce7c4f4aedf
samsks <samuel.meireles.15s@outlook.com>
2023-10-27 10:47:26 -0300

    ci: testing error case in push action.

--- math.go
+++ math.go
@@ -1,11 +1,12 @@
 package main
+
 import "fmt"
 
 func main() {
-	fmt.Println(SumInt(10, 11))
+	fmt.Println(sumInt(10, 11))
 }
 
 // Sum is a function that returns the sum of two integers
-func SumInt(a int, b int) int {
+func sumInt(a int, b int) int {
 	return a + b
 }
